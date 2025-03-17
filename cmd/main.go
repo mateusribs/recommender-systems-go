b1e806d919a4c16f25cf90489c88b5352e8b6484
mateusribs <mateus.ribe.sva@gmail.com>
2025-03-17 20:49:34 -0300

    finish recommender algorithm using item-item approach

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/mateusribs/recommender-systems-go/internal/algorithms"
 	"github.com/mateusribs/recommender-systems-go/internal/database"
 )
 
@@ -11,7 +10,7 @@ func main() {
     fmt.Println("Olá, Golang!")
 
 	ratings := database.LoadRatings()
-	// movies := database.LoadMovies()
+	movies := database.LoadMovies().Select([]int{0, 1}).Maps()
 
 	n_ratings := ratings.Nrow()
 	n_users := len(ratings.GroupBy("userId").GetGroups())
@@ -22,24 +21,7 @@ func main() {
 	
 	uMatrix := database.NewUtilityMatrix(&ratings)
 
-	fmt.Println(uMatrix.FindSimilarMovies(15, 10))
+	recommendations := uMatrix.FindSimilarMovies(1, 10, movies)
 
-	dataset := [][]float64{
-		{2.7810836,2.550537003},
-		{1.465489372,2.362125076},
-		{3.396561688,4.400293529},
-		{1.38807019,1.850220317},
-		{3.06407232,3.005305973},
-		{7.627531214,2.759262235},
-		{5.332441248,2.088626775},
-		{6.922596716,1.77106367},
-		{8.675418651,-0.242068655},
-		{7.673756466,3.508563011},
-	}
-
-	knn := algorithms.KNN{}
-
-	fmt.Println(knn.GetNeighbors(dataset, dataset[0], 7))
-
-
+	fmt.Println(recommendations)
 }
--- internal/database/utility_matrix.go
+++ internal/database/utility_matrix.go
@@ -1,20 +1,21 @@
 package database
 
 import (
-	"fmt"
 	"slices"
 	"sort"
 
 	"github.com/go-gota/gota/dataframe"
-	"github.com/james-bowman/sparse"
+	"github.com/mateusribs/recommender-systems-go/internal/algorithms"
 )
 
 
 type UtilityMatrix struct {
 	df* dataframe.DataFrame
 	UserMapper map[int]int
+	UserMapperInv map[int]int
 	MovieMapper map[int]int
-	Matrix *sparse.CSR
+	MovieMapperInv map[int]int
+	Matrix [][]float64
 }
 
 
@@ -43,36 +44,86 @@ func (u *UtilityMatrix) setMapper (ids []int) map[int]int {
 	return mapper
 }
 
-func (u *UtilityMatrix) createSparseMatrix (userIds []int, movieIds []int) *sparse.CSR {
-	rows := len(u.UserMapper)
-	cols := len(u.MovieMapper)
+func (u *UtilityMatrix) setMapperInv (ids []int) map[int]int {
+	N := len(ids)
+	mapper := make(map[int]int)
 
-	data := u.df.Col("rating").Float()
+	for i := 0; i < N; i++ {
+		k := ids[i]
+		mapper[i] = k
+	}
 
-	return sparse.NewCSR(rows, cols, userIds, movieIds, data)
+	return mapper
 }
 
-func (u *UtilityMatrix) FindSimilarMovies(movie_id int, k int) []int {
-	neighbors_ids := make([]int, k + 1)
+func (u *UtilityMatrix) createSparseMatrix (userIds []int, movieIds []int) [][]float64 {
+	rows := len(movieIds)
+	cols := len(userIds)
+
+	matrix := make([][]float64, rows)
+
+	for i := range matrix {
+		matrix[i] = make([]float64, cols)
+		for j := range matrix[i] {
+			matrix[i][j] = 0
+		}
+	}
+
+	data := u.df.Select([]string{"movieId", "userId", "rating"}).Maps()
+	
+	for _, row := range data {
+		userId, _ := row["userId"].(int)
+		movieId, _ := row["movieId"].(int)
+		n := u.UserMapper[userId]
+		m := u.MovieMapper[movieId]
+		if rating, ok := row["rating"].(float64); ok {
+			matrix[m][n] = rating
+		}
+	}
+
+	return matrix
+}
+
+func (u *UtilityMatrix) FindSimilarMovies(movie_id int, k int, movies []map[string]interface{}) []string {
+	knn := algorithms.KNN{}
 
 	movie_ind := u.MovieMapper[movie_id]
-	movie_vec := u.Matrix.RowView(movie_ind)
+	movie_vec := u.Matrix[movie_ind]
 
-	fmt.Println(movie_ind, movie_vec)
+	neighbors := knn.GetNeighbors(u.Matrix, movie_vec, k + 1)
+	neighbors = neighbors[1:]
 
-	return neighbors_ids
+	moviesTitles := make(map[int]string)
+
+	for _, mapper := range movies {
+		
+		movieId, _ := mapper["movieId"].(int)
+		movieTitle, _ := mapper["title"].(string)
+		moviesTitles[movieId] = movieTitle
+	}
+
+	neighborsTitles := make([]string, len(neighbors))
+
+	for i, movieId := range neighbors {
+		id := u.MovieMapperInv[movieId]
+		neighborsTitles[i] = moviesTitles[id]
+	}
+
+	return neighborsTitles
 }
 
 
-func NewUtilityMatrix(df* dataframe.DataFrame) *UtilityMatrix {
+func NewUtilityMatrix(data *dataframe.DataFrame) *UtilityMatrix {
 
-	uMatrix := &UtilityMatrix{df: df}
+	uMatrix := &UtilityMatrix{df: data}
 
 	uniqueMoviesIds := uMatrix.getUniqueIds("movieId")
 	uniqueUsersIds := uMatrix.getUniqueIds("userId")
 
 	uMatrix.UserMapper = uMatrix.setMapper(uniqueUsersIds)
 	uMatrix.MovieMapper = uMatrix.setMapper(uniqueMoviesIds)
+	uMatrix.UserMapperInv = uMatrix.setMapperInv(uniqueUsersIds)
+	uMatrix.MovieMapperInv = uMatrix.setMapperInv(uniqueMoviesIds)
 	uMatrix.Matrix = uMatrix.createSparseMatrix(uniqueUsersIds, uniqueMoviesIds)
 
 	return uMatrix
